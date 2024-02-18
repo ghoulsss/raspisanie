66ba5bded332302bc02280cbb7993fc6f7b50980
Pavlov Danil <danpavkzn@mail.ru>
2024-02-18 19:37:23 +0300

    половина расписания готова

--- main.py
+++ main.py
@@ -3,16 +3,9 @@ import telebot
 bot = telebot.TeleBot('6765117292:AAEDJttvGWNjCSYyZhAXzzERR1579sKtA_E')
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['расп'])
 def main(message):
     bot.send_message(message.chat.id, 'кгасу не \'_\', сиди кайфуй')
-
-
-@bot.message_handler(commands=['who'])
-def who(message):
-    bot.send_message(message.chat.id,
-                     '')
-    bot.send_photo(message.chat.id, )
 
 
 bot.infinity_polling()
