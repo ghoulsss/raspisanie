573ab9028f2b5677ad03af2b573e3ae443163087
Pavlov Danil <danpavkzn@mail.ru>
2024-09-17 15:51:38 +0300

    lets go

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
-import os
-import time
-
-os.environ["TZ"] = "Russia/Moscow"
-time.tzset()
-
 import telebot
 from vremya import *
-import schedule
 import time
 import threading
+import schedule
 
 bot = telebot.TeleBot('6765117292:AAEDJttvGWNjCSYyZhAXzzERR1579sKtA_E')
 
 
 def send_scheduled_message():
     chat_id = "-1002026154974"
-    message = ReadRasp.pull_raspisanie(TypeWeek.is_even_week(), TypeWeek.current_day)
-    if TypeWeek.current_day in ('Суббота', 'Воскресенье'):
-        message = 'Сегодня выходной'
+    message = Raspisanie.pull_raspisanie()
     bot.send_message(chat_id, message)
 
 
-def send_scheduled_message_tomorrow():
-    chat_id = "-1002026154974"
-    message = ReadRasp.pull_raspisanie(TypeWeek.is_even_week(TypeWeek.tomorrow_date),
-                                       TypeWeek.days[datetime.now(pytz.timezone(
-                                           'Europe/Moscow')).weekday() + 2])
-    if TypeWeek.days[datetime.now(pytz.timezone('Europe/Moscow')).weekday() + 2] in ('Суббота', 'Воскресенье'):
-        message = 'Завтра выходной'
-    bot.send_message(chat_id, message)
+# def send_scheduled_message_tomorrow():
+#     chat_id = "-1002026154974"
+#     message = Raspisanie.pull_raspisanie()
+#     bot.send_message(chat_id, message)
 
 
 def scheduled_task():
-    # Запускаем задачу каждый день в 7:00
+    # Запускаем задачу каждый день в 7 и в 8 утра
     schedule.every().day.at("04:00").do(send_scheduled_message)
-    schedule.every().day.at("17:00").do(send_scheduled_message_tomorrow)
+    # schedule.every().day.at("17:00").do(send_scheduled_message_tomorrow)
     while True:
         schedule.run_pending()
         time.sleep(1)
@@ -43,24 +31,25 @@ def scheduled_task():
 def main():
     @bot.message_handler(commands=['today'])
     def start(message):
-        bot.send_message(message.chat.id, ReadRasp.pull_raspisanie(TypeWeek.is_even_week(), TypeWeek.current_day))
+        bot.send_message(message.chat.id, Raspisanie.pull_raspisanie())
         print(1)
 
     @bot.message_handler(commands=['tomorrow'])
     def start(message):
-        bot.send_message(message.chat.id, ReadRasp.pull_raspisanie(TypeWeek.is_even_week(TypeWeek.tomorrow_date),
-                                                                   TypeWeek.days[datetime.now(pytz.timezone(
-                                                                       'Europe/Moscow')).weekday() + 2]))
+        # bot.send_message(message.chat.id, Raspisanie.pull_raspisanie())
+        bot.send_message(message.chat.id, 'пока не настроил')
         print(1)
 
     @bot.message_handler(commands=['week'])
     def start(message):
-        bot.send_message(message.chat.id, ReadRasp.pull_raspisanie_nedelya(TypeWeek.is_even_week()))
+        # bot.send_message(message.chat.id, Raspisanie.pull_raspisanie_nedelya())
+        bot.send_message(message.chat.id, 'пока не настроил')
         print(1)
 
     @bot.message_handler(commands=['next_week'])
     def start(message):
-        bot.send_message(message.chat.id, ReadRasp.pull_raspisanie_next_nedelya(TypeWeek.is_even_week()))
+        # bot.send_message(message.chat.id, Raspisanie.pull_raspisanie_next_nedelya(TypeWeek.is_even_week()))
+        bot.send_message(message.chat.id, 'пока не настроил')
         print(1)
 
     thread = threading.Thread(target=scheduled_task)
--- vremya.py
+++ vremya.py
@@ -1,127 +1,14 @@
-from datetime import datetime
 import pytz
-import json
 
 
-class TypeWeek:
-    days = {1: 'Понедельник',
-            2: 'Вторник',
-            3: 'Среда',
-            4: 'Четверг',
-            5: 'Пятница',
-            6: 'Суббота',
-            7: 'Воскресенье'}
-    moscow_timezone = pytz.timezone('Europe/Moscow')
-    current_day = days[datetime.now(moscow_timezone).weekday() + 1]  # Понедельник
-    date = f"{datetime.now().day:02d}.{datetime.now().month:02d}"  # 19.01
-    tomorrow_date = f"{datetime.now().day + 1:02d}.{datetime.now().month:02d}"  # 20.01
-
+class Raspisanie:
+    day =
     @staticmethod
-    def is_even_week(date=date) -> str:
-        date_object = datetime.strptime(date, "%d.%m")
-
-        week_number = date_object.isocalendar()[1]
-        return 'Chet' if week_number % 2 == 0 else 'Nechet'
-
-
-class ReadRasp:
-    @staticmethod
-    def pull_raspisanie(type_week, day, filename='2grupa.json'):
-        with open(filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-
-        schedule = data.get(type_week, {})
-        sending_message = ''
-
-        if day in ('Суббота', 'Воскресенье'):
-            return 'Зачилься выходные же'
-
-        monday_schedule = schedule.get(day, [])
-        #monday_schedule = schedule.get('Среда', [])
-        for i, lesson in enumerate(monday_schedule, start=1):
-            lesson_time = lesson.get("Время")
-            subject = lesson.get("Предмет", "")
-            teacher = lesson.get("Преподаватель", "")
-            lesson_type = lesson.get("Тип", "")
-            classroom = lesson.get("Аудитория", "")
-
-            if i != len(monday_schedule):
-                if subject == 'Физ-ра':
-                    sending_message += f"{i}) {lesson_time}, {subject}\n\n"
-                else:
-                    sending_message += f"{i}) {lesson_time}, {subject}, {teacher}, {lesson_type}, {classroom}\n\n"
-            else:
-                if subject == 'Физ-ра':
-                    sending_message += f"{i}) {lesson_time}, {subject}"
-                else:
-                    sending_message += f"{i}) {lesson_time}, {subject}, {teacher}, {lesson_type}, {classroom}"
-
-        return sending_message
-
-    @staticmethod
-    def pull_raspisanie_nedelya(type_week, filename='2grupa.json'):
-        with open(filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-
-        schedule = data.get(type_week, {})
-        sending_message = 'Нечетная\n' if type_week == 'Nechet' else 'Четная\n'
-        sending_message += ''
-
-        # Перебираем дни недели
-        days_of_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
-        for day in days_of_week:
-            sending_message += f"{day}:\n"
-            day_schedule = schedule.get(day, [])
-
-            # Перебираем уроки в текущем дне
-            for i, lesson in enumerate(day_schedule, start=1):
-                lesson_time = lesson.get("Время")
-                subject = lesson.get("Предмет", "")
-                teacher = lesson.get("Преподаватель", "")
-                lesson_type = lesson.get("Тип", "")
-                classroom = lesson.get("Аудитория", "")
-                if subject == 'Физ-ра':
-                    sending_message += f"{i}) {lesson_time}, {subject}\n"
-                else:
-                    sending_message += f"{i}) {lesson_time}, {subject}, {teacher}, {lesson_type}, {classroom}\n"
-
-            if day != 'Пятница':
-                sending_message += "\n"
-
-        return sending_message
-
-    @staticmethod
-    def pull_raspisanie_next_nedelya(type_week, filename='2grupa.json'):
-        with open(filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-        type_week = 'Nechet' if type_week == 'Chet' else 'Chet'
-        schedule = data.get(type_week, {})
-        sending_message = 'Нечетная\n' if type_week == 'Nechet' else 'Четная\n'
-        sending_message += ''
-
-        # Перебираем дни недели
-        days_of_week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
-        for day in days_of_week:
-            sending_message += f"{day}:\n"
-            day_schedule = schedule.get(day, [])
-
-            # Перебираем уроки в текущем дне
-            for i, lesson in enumerate(day_schedule, start=1):
-                lesson_time = lesson.get("Время")
-                subject = lesson.get("Предмет", "")
-                teacher = lesson.get("Преподаватель", "")
-                lesson_type = lesson.get("Тип", "")
-                classroom = lesson.get("Аудитория", "")
-                if subject == 'Физ-ра':
-                    sending_message += f"{i}) {lesson_time}, {subject}\n"
-                else:
-                    sending_message += f"{i}) {lesson_time}, {subject}, {teacher}, {lesson_type}, {classroom}\n"
-
-            if day != 'Пятница':
-                sending_message += "\n"
-
-        return sending_message
+    def pull_raspisanie():
+        day = 1 #google спарсить
+        raspisanie = 1 # работа с бд
+        print(raspisanie)
 
 
-#print(TypeWeek.current_day, TypeWeek.date)
-# print(ReadRasp.pull_raspisanie(TypeWeek.is_even_week(TypeWeek.tomorrow_date), TypeWeek.days[1]))
+print(Raspisanie.pull_raspisanie())
+
